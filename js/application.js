6009bb21551a09e849465b15405e333e9e4bbddf
Raymond Sapida <ray.sapida@gmail.com>
2016-06-09 15:16:31 -0700

    Add separate scores for each player

--- js/application.js
+++ js/application.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1000, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
 
@@ -17,15 +17,14 @@ var cursors;
 
 var stars;
 var score = 0;
+var score2 = 0;
 var scoreText;
+var scoreText2;
 
 function create() {
-
-  //  We're going to be using physics, so enable the Arcade Physics system
   game.physics.startSystem(Phaser.Physics.ARCADE);
-
-  //  A simple background for our game
-  game.add.sprite(0, 0, 'sky');
+  game.stage.backgroundColor = "#4488AA";
+  //game.add.sprite(0, 0, 'sky');
 
   //  The platforms group contains the ground and the 2 ledges we can jump on
   platforms = game.add.group();
@@ -37,7 +36,7 @@ function create() {
   var ground = platforms.create(0, game.world.height - 64, 'ground');
 
   //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-  ground.scale.setTo(2, 2);
+  ground.scale.setTo(4, 4);
 
   //  This stops it from falling away when you jump on it
   ground.body.immovable = true;
@@ -46,7 +45,10 @@ function create() {
   var ledge = platforms.create(400, 400, 'ground');
   ledge.body.immovable = true;
 
-  ledge = platforms.create(-150, 250, 'ground');
+  ledge = platforms.create(-200, 250, 'ground');
+  ledge.body.immovable = true;
+
+  ledge = platforms.create(500, 200, 'ground');
   ledge.body.immovable = true;
 
   // The player and its settings
@@ -91,7 +93,8 @@ function create() {
   }
 
   //  The score
-  scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+  scoreText = game.add.text(16, 16, 'player 1 score: 0', { fontSize: '28px', fill: '#000' });
+  scoreText2 = game.add.text(16, 40, 'player 2 score: 0', { fontSize: '28px', fill: '#000' });
 
   //  Our controls.
   cursors = game.input.keyboard.createCursorKeys();
@@ -108,6 +111,7 @@ function update() {
 
   //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
   game.physics.arcade.overlap(player, stars, collectStar, null, this);
+  game.physics.arcade.overlap(player2, stars, collectStar, null, this);
 
   //  Reset the players velocity (movement)
   player.body.velocity.x = 0;
@@ -172,7 +176,11 @@ function collectStar (player, star) {
   star.kill();
 
   //  Add and update the score
-  score += 10;
-  scoreText.text = 'Score: ' + score;
-
+  if (player == player2) {
+    score2 += 10
+    scoreText2.text = 'Player 2 Score: ' + score2;
+  } else {
+    score += 10;
+    scoreText.text = 'Player 1 Score: ' + score;
+  }
 }
